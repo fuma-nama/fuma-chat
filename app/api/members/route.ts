dc1198d11443d0e54ecc72470b5b9991763b9716
fuma-nama <god63820869@gmail.com>
2024-04-20 21:06:58 +0800

    Implement Members API

---
+++ app/api/members/route.ts
@@ -0,0 +1,45 @@
+import { memberTable } from "@/lib/database/schema";
+import { db } from "@/lib/server/db";
+import { handler, requireAuth, validate } from "@/lib/server/route-handler";
+import { API } from "@/lib/server/types";
+import { getMembers } from "@/lib/server/zod";
+import { clerkClient } from "@clerk/nextjs/server";
+import { eq } from "drizzle-orm";
+import { NextResponse } from "next/server";
+
+export const GET = handler<API["/api/members:get"]["data"]>(async (req) => {
+  const { userId } = requireAuth();
+
+  const data = await validate(req, getMembers, "params");
+  const members = await db
+    .select()
+    .from(memberTable)
+    .where(eq(memberTable.channelId, data.channelId));
+
+  if (members.every((m) => m.userId !== userId))
+    return NextResponse.json(
+      { message: "You must join the group to see its members" },
+      { status: 401 }
+    );
+
+  const users = await clerkClient.users.getUserList({
+    userId: members.map((m) => m.userId),
+  });
+  const userMap = new Map(users.map((u) => [u.id, u]));
+
+  return NextResponse.json(
+    members.flatMap((m) => {
+      const user = userMap.get(m.userId);
+      if (!user) return [];
+
+      return {
+        user: {
+          id: user.id,
+          imageUrl: user.imageUrl,
+          name: `${user.firstName} ${user.lastName}`,
+        },
+        permissions: m.permissions,
+      };
+    })
+  );
+});
